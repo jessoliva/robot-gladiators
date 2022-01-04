5d0bf69569f02a894b0942ff10087425bd71083d
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-03 19:08:50 -0600

    initialized project structure

---
+++ assets/js/game.js
@@ -0,0 +1,5 @@
+window.alert("This is an alert! JavaScript is running!");
+// when you open browser, this alert will pop up, and can't do anything until you acknowledge the dialog window and click the button
+
+
+
