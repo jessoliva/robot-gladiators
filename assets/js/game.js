7be33411ac582fbd24f30d87163d14671a646ea3
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-04 21:06:21 -0600

    completed 3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,27 @@
-window.alert("This is an alert! JavaScript is running!");
+// window.alert("This is an alert! JavaScript is running!");
 // when you open browser, this alert will pop up, and can't do anything until you acknowledge the dialog window and click the button
 
+var playerName = window.prompt("What is your robot's name?");
+
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// // Note the lack of quotation marks around playerName
+// window.alert(playerName);
+// // this alert will contain the playerName entered
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+// fight end
 
 
