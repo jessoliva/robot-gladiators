6e6409832c35a7855618a57be8c3259468009f93
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-06 15:21:29 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,34 +67,68 @@ var fight = function(enemyName) { //enemyName is just a parameter
         else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-    
-        // if (promptFight !== "skip" || promptFight !== "SKIP" || promptFight !== "fight" || promptFight !== "FIGHT") {
-        //     window.alert("You need to choose a valid option. Try again!");
-        //     fight();
-        // }
     };
 };
 
-// fight each enemy-robot by looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    
-        // pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames[i];
-    
-        // reset enemyHealth before starting new fight
-        enemyHealth = 50;
-    
-        // use debugger to pause script from running and check what's going on at that moment in the code
-        // debugger;
-    
-        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName);
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    //debugger;
+    // fight each enemy-robot by looping over them and fighting them one at a time
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        
+            // pick new enemy to fight based on the index of the enemyNames array
+            var pickedEnemyName = enemyNames[i];
+        
+            // reset enemyHealth before starting new fight
+            enemyHealth = 20;
+        
+            // use debugger to pause script from running and check what's going on at that moment in the code
+            // debugger;
+        
+            // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        } 
     }
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+};
+
+// start the game when the page loads
+startGame();
+// note: w/o this call, you wouldn't go beyond naming your robot
+
+// note: startGame(); (now endGame();) was called within the function itself bc after the player has died, and after you break out of the function loop, you run through the startGame() function again but this time go to "else" and have the alert Game Over! pop up
+
+// function to end the entire game
+function endGame() { // changed from var endGame = function() {...} bc calling endGame(); higher up!!
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     } 
-}
+    else {
+      window.alert("You've lost your robot in battle.");
+    }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+    // restart the game
+    startGame();
+    } 
+    else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
