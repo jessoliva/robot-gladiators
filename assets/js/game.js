f034eb6a9fa55e52e69c908f77814663ed8de196
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-07 21:32:03 -0600

    high score feature completed

--- assets/js/game.js
+++ assets/js/game.js
@@ -168,7 +168,29 @@ var startGame = function() {
 var endGame = function() { // changed from var endGame = function() {...} bc calling endGame(); higher up!!
     // if player is still alive, player wins!
     if (playerInfo.health > 0) {
-      window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+
+        // check localStorage for high score, if it's not there, use 0
+        var highScore = localStorage.getItem("highscore");
+
+        if (highScore === null) {
+            highScore = 0;
+        }
+        // shorthand is: highScore = highScore || 0;
+        // short circuit conditional statement
+        // 
+
+        // if player has more money than the high score, player has new high score!
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } 
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
+
     } 
     else {
       window.alert("You've lost your robot in battle.");
